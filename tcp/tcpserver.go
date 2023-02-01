69b7ce4d4fb80ecd55784259b44b741a66bb37da
26530 <2653084650@qq.com>
2023-02-01 21:20:34 +0800

    完成tcp服务器

--- tcp/tcpserver.go
+++ tcp/tcpserver.go
@@ -1,10 +1,12 @@
 package tcp
 
 import (
+	"context"
 	logger "mygodis/log"
 	"net"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
 )
@@ -31,4 +33,37 @@ func ListenAndServeWithSignal(config *Config, handler Handler) error {
 		return err
 	}
 	logger.Info("bind: %s, start listening...", config.Address)
+	listenAndServe(listener, handler, closeC)
+	return nil
+}
+func listenAndServe(listener net.Listener, handler Handler, closeC <-chan struct{}) {
+	errorC := make(chan error)
+	defer close(errorC)
+	go func() {
+		select {
+		case <-closeC:
+			logger.Info("get exit signal")
+		case er := <-errorC:
+			logger.Error("error: %s", er)
+		}
+		logger.Info("server closed")
+	}()
+	_ = listener.Close()
+	_ = handler.Close()
+	ctx := context.Background()
+	wt := sync.WaitGroup{}
+	for {
+		accept, err := listener.Accept()
+		if err != nil {
+			errorC <- err
+			break
+		}
+		logger.Info("accept: %s", accept.RemoteAddr())
+		wt.Add(1)
+		go func() {
+			defer wt.Done()
+			handler.Handle(ctx, accept)
+		}()
+	}
+	wt.Wait()
 }
